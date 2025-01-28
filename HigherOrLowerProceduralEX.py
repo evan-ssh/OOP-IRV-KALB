d8a5cd76438b8b06a5ffb86276c15c97e25d22b1
evan-ssh <62630880+evan-ssh@users.noreply.github.com>
2025-01-27 23:05:21 -0330

    Higher or Lower using procedural

--- HigherOrLowerProceduralEX.py
+++ HigherOrLowerProceduralEX.py
@@ -16,11 +16,52 @@ def Shuffle(deckListin):
 
 print("Welcome to higher or lower")
 print("Choose whether the next card to be shown will be higher or lower than current card")
-print("Right answers = +20 points, and a wrong answer = -15")
+print("Right answers = +20 points, wrong answer = -15")
 print("Begin with 50 points")
 
 startingDeckList = []
 for suit in suits:
-    for value, rank in enumerate(ranks)
+    for value, rank in enumerate(ranks):
         cardDict = {'rank':rank,'suit':suit, 'value':value+1}
         startingDeckList.append(cardDict)
+score = 50
+
+while True:
+    print()
+    gameDeckList = Shuffle(startingDeckList)
+    currentCardDict = GetCard(gameDeckList)
+    currentCardRank = currentCardDict['rank']
+    currentCardValue = currentCardDict['value']
+    currentCardSuit = currentCardDict['suit']
+    print(f"Starting card is: {currentCardRank} of {currentCardSuit}\n")
+
+    for cardNumber in range(0,NCARDS):
+        answer = input(f"Will the next card be higher or lower than {currentCardRank} of {currentCardSuit}? (enter h or l)").casefold()
+        nextCardDict = GetCard(gameDeckList)
+        nextCardRank = nextCardDict['rank']
+        nextCardSuit = nextCardDict['suit']
+        nextCardValue = nextCardDict['value']
+        print(f"Next card is: {nextCardRank} of {nextCardSuit}")
+
+        if answer == "h":
+            if nextCardValue > currentCardValue:
+                print("Correct, It Was Higher! +20")
+                score += 20
+            else:
+                print("Incorrect, It Was Lower -15")
+                score -= 15
+
+        elif answer == "l":
+            if nextCardValue < currentCardValue:
+                print("Correct, It Was Lower! +20")
+                score += 20
+            else:
+                print("Incorrect, It Was Higher -15")
+                score -= 15
+
+        print(f"Your Score Is:{score}\n")
+        currentCardValue = nextCardValue
+        currentCardSuit = nextCardSuit
+    goAgain = input("To player again press 'ENTER' or 'q' to quit")
+    if goAgain == 'q':
+        break

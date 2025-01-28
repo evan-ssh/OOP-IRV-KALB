44bb0af16a86d3a67edd73ba4586d7e7de2a5ad2
evan-ssh <62630880+evan-ssh@users.noreply.github.com>
2025-01-27 22:43:46 -0330

    creating deck

--- HigherOrLowerProceduralEX.py
+++ HigherOrLowerProceduralEX.py
@@ -13,3 +13,14 @@ def Shuffle(deckListin):
     deckListOut = deckListin.copy()
     random.shuffle(deckListOut)
     return deckListOut
+
+print("Welcome to higher or lower")
+print("Choose whether the next card to be shown will be higher or lower than current card")
+print("Right answers = +20 points, and a wrong answer = -15")
+print("Begin with 50 points")
+
+startingDeckList = []
+for suit in suits:
+    for value, rank in enumerate(ranks)
+        cardDict = {'rank':rank,'suit':suit, 'value':value+1}
+        startingDeckList.append(cardDict)

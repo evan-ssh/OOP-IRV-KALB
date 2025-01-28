286ed61bd1a8de63044c94e3d690db99cba1adee
evan-ssh <62630880+evan-ssh@users.noreply.github.com>
2025-01-27 22:38:20 -0330

    Get card

---
+++ HigherOrLowerProceduralEX.py
@@ -0,0 +1,10 @@
+import random
+
+suits = ("spades","Hearts","Clubs","Diamonds")
+ranks = ("Ace","2","3","4","5","6","7","8","9","10","Jack","Queen","King")
+
+NCARDS = 8
+
+def GetCard(deckListin):
+    thisCard = deckListin.pop()
+    return thisCard

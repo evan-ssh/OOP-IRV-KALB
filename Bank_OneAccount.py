cda67b8848cd48f000ade5ebf07e13818ed7fd35
evan-ssh <62630880+evan-ssh@users.noreply.github.com>
2025-01-28 22:29:37 -0330

    Get Account Balnce With Password

--- Bank_OneAccount.py
+++ Bank_OneAccount.py
@@ -3,8 +3,20 @@ accountBalance = 100
 accountPassword = "apple"
 
 while True:
-    print("Press b to get account balance")
+    
+    print("\nPress b to get account balance")
     print("Press d to make deposit")
     print("Press w to make a withdrawal")
     print("Press s to show the account")
-    print("Press q to quit")
+    print("Press q to quit\n")
+
+    command = input(f"|Enter Command|\n").lower()
+    command = command[0]#Use first letter
+    
+    if command == "b":
+        print("Password Required To Display Balance")
+        userPassword = input("Enter accounts password: ").strip()
+        if userPassword != accountPassword:
+            print("\nInvalid password")
+        else:
+            print(f"\nAccount Balance: {accountBalance}")

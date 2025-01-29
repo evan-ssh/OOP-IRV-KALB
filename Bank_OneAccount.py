24fcf1e213653f3250e88adea1d86be1ba4a2b78
evan-ssh <62630880+evan-ssh@users.noreply.github.com>
2025-01-28 22:18:55 -0330

    menu and staring variables

---
+++ Bank_OneAccount.py
@@ -0,0 +1,10 @@
+accountName = "Tim"
+accountBalance = 100
+accountPassword = "apple"
+
+while True:
+    print("Press b to get account balance")
+    print("Press d to make deposit")
+    print("Press w to make a withdrawal")
+    print("Press s to show the account")
+    print("Press q to quit")

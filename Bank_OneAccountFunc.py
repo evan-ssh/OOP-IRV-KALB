9e6d3e7dfb36ab2a7b482a0e49acf93faa9b1849
evan-ssh <62630880+evan-ssh@users.noreply.github.com>
2025-01-29 22:56:47 -0330

    define new account

---
+++ Bank_OneAccountFunc.py
@@ -0,0 +1,9 @@
+accountName = ''
+accountBalance = 0
+accountPassword = ''
+
+def newAccount(name,balance,password):
+    global accountName,accountBalance,accountPassword
+    accountName = name
+    accountBalance = balance
+    accountPassword = password

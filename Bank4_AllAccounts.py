8199f9c59948c57295345ee9b3cb39765a8acaa6
evan-ssh <62630880+evan-ssh@users.noreply.github.com>
2025-01-31 12:19:30 -0330

    make a deposit

--- Bank4_AllAccounts.py
+++ Bank4_AllAccounts.py
@@ -21,3 +21,13 @@ def getBalance(accountNumber,password):
         print("Invalid Password")
         return
     return accountBalancesList[accountNumber]
+
+def deposit(accountNumber, amountToDeposit, password):
+    global accountnamesList, accountBalancesList, accountPasswordsList
+    if password != accountPasswordsList[accountNumber]:
+        print("Incorrect Password") 
+    if amountToDeposit < 0:
+        print("Invalid Amount")
+        return
+    accountBalancesList[accountNumber] += amountToDeposit
+    return accountBalancesList[accountNumber]

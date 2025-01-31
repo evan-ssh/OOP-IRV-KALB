af3706236a70590260e07fd00a6f3bd8278468f6
evan-ssh <62630880+evan-ssh@users.noreply.github.com>
2025-01-30 23:01:21 -0330

    show accounts

--- Bank4_AllAccounts.py
+++ Bank4_AllAccounts.py
@@ -7,4 +7,10 @@ def newAccount(name,balance,password):
     accountnamesList.append(name)
     accountBalancesList.append(balance)
     accountPasswordsList.append(password)
-    
+
+def showAccount(accountNumber):
+    global accountnamesList, accountBalancesList, accountPasswordsList
+    print(f"Account number {accountNumber}")
+    print(f"Account name {accountnamesList[accountNumber]}")
+    print(f"Account balance {accountBalancesList[accountNumber]}")
+    print(f"Account password {accountPasswordsList[accountNumber]}")

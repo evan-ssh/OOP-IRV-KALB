d2475f160213ffeed60e2b147daca677e36781fa
evan-ssh <62630880+evan-ssh@users.noreply.github.com>
2025-02-01 15:59:50 -0330

    Get Account Balance

--- Bank5_Dictonary.py
+++ Bank5_Dictonary.py
@@ -9,7 +9,15 @@ def showAccount(accountnumber):
     global accountsList
     AccountDict = accountsList[accountnumber]
     print(f"Account Number:{accountnumber}")
-    print(f"Account Nme:{AccountDict['name']}")
+    print(f"Account Name:{AccountDict['name']}")
     print(f"Acccount Balance: {AccountDict['balance']}")
     print(f"Account Password: {AccountDict['password']}")
 
+def getBalance(accountnumber,password):
+    global accountsList
+    accountDict = accountsList[accountnumber]
+    if password != accountDict['password']:
+        print("Invalid Password")
+        return
+    else:
+        print(f"Account Balance:{accountDict['balance']}")

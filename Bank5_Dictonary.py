95ce2248a55d2fe03f9272ae7d5f8cec9856f59e
evan-ssh <62630880+evan-ssh@users.noreply.github.com>
2025-02-01 15:53:35 -0330

    show an account

--- Bank5_Dictonary.py
+++ Bank5_Dictonary.py
@@ -1,6 +1,15 @@
 accountsList = []
 
 def newAccount(name,balance,password):
-    global accountslist
+    global accountsList
     accountDict = {"name":name,"balance":balance,"password":password}
     accountsList.append(accountDict)
+
+def showAccount(accountnumber):
+    global accountsList
+    AccountDict = accountsList[accountnumber]
+    print(f"Account Number:{accountnumber}")
+    print(f"Account Nme:{AccountDict['name']}")
+    print(f"Acccount Balance: {AccountDict['balance']}")
+    print(f"Account Password: {AccountDict['password']}")
+

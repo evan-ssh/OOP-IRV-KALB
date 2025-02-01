0090f1a605121f58bc91c4949956a8469da7d7f0
evan-ssh <62630880+evan-ssh@users.noreply.github.com>
2025-02-01 20:09:40 -0330

    Method to Deposit

--- Account.py
+++ Account.py
@@ -3,3 +3,13 @@ class Account:
         self.name = name
         self.balance = int(balance)
         self.password = password
+
+def deposit(self,amountToDeposit,password):
+    if password != self.password:
+        print("Incorrect Password")
+        return None
+    if amountToDeposit < 0:
+        print("You cannot deposit a negative amount")
+        return None
+    self.balance += amountToDeposit
+    return self.balance

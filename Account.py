66efac03c71a02829358e1c8034e7aba06b1c2ef
evan-ssh <62630880+evan-ssh@users.noreply.github.com>
2025-02-01 20:13:46 -0330

    Method To Withdraw From Accoun

--- Account.py
+++ Account.py
@@ -13,3 +13,17 @@ def deposit(self,amountToDeposit,password):
         return None
     self.balance += amountToDeposit
     return self.balance
+
+def withdraw(self,amountToWithdraw,password):
+    if password != self.password:
+        print("Incorrect Password")
+        return None
+    if amountToWithdraw > self.balance:
+        print("You cannot withdraw more than what is in your account")
+        return None
+    if amountToWithdraw < 0:
+        print("You cannot withdraw a negative amount")
+        return None
+    self.balance -= amountToWithdraw
+    return self.balance
+

5fcc4c8978b363e0221828179b92865e721f6fd1
evan-ssh <62630880+evan-ssh@users.noreply.github.com>
2025-02-01 20:51:41 -0330

    using 2 instances of a  light switch

--- LightSwitchTwoInstances.py
+++ LightSwitchTwoInstances.py
@@ -10,3 +10,17 @@ class LightSwitch():
     
     def showSwitchState(self):
         print(f"The switch is on {self.SwitchIsOn}")
+    
+if __name__ == "__main__":
+    light1 = LightSwitch()
+    light2 = LightSwitch()
+    light1.showSwitchState()
+    light2.showSwitchState()
+    light1.turnON()
+    light2.turnON()
+    light1.showSwitchState()
+    light2.showSwitchState()
+    light1.turnOFF()
+    light2.turnOFF()
+    light1.showSwitchState()
+    light2.showSwitchState()

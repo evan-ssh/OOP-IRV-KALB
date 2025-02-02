a4450e4a9f970081832dc34bac1a0d77f2d0e508
evan-ssh <62630880+evan-ssh@users.noreply.github.com>
2025-02-01 20:37:31 -0330

    Light Switch on or off

---
+++ LightSwitch.py
@@ -0,0 +1,19 @@
+class LightSwitch():
+    def __init__(self):
+        self.SwitchIsOn = False
+
+    def turnON(self):
+        self.SwitchIsOn = True
+    
+    def turnOFF(self):
+        self.SwitchIsOn = False
+
+    def showSwitchState(self):
+        print(f"The switch is on {self.SwitchIsOn}")
+if __name__ == "__main__":
+    switch = LightSwitch()
+    switch.showSwitchState()
+    switch.turnON()
+    switch.showSwitchState()
+    switch.turnOFF()
+    switch.showSwitchState()
